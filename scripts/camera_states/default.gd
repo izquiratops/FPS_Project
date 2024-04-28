c5f885d4a4e4fe0b38c20501e3acdddba2dd3219
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2024-04-28 12:57:42 +0200

    Moving free-look listener to handle_input

--- scripts/camera_states/default.gd
+++ scripts/camera_states/default.gd
@@ -7,9 +7,6 @@ func update(delta) -> void:
 	# Apply camera tilt
 	camera_3d.rotation.z = lerp(camera_3d.rotation.z, player.current_camera_tilt, delta * player.lerp_speed)
 
-	if Input.is_action_pressed("free-look"):
-		emit_signal("change_state_request", "FreeLook")
-
 func handle_input(event) -> void:
 	if event is InputEventMouseMotion:
 		# Looking left/right
@@ -18,3 +15,6 @@ func handle_input(event) -> void:
 		head.rotate_x((deg_to_rad( - 1 * player.mouse_sensitivity * event.relative.y)))
 		# Do not let the player look over 85rads up or down.
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad( - 85), deg_to_rad(85))
+
+	if Input.is_action_pressed("free-look"):
+		emit_signal("change_state_request", "FreeLook")
--- scripts/camera_states/free_look.gd
+++ scripts/camera_states/free_look.gd
@@ -3,6 +3,6 @@ extends CameraState
 func init(_data: Dictionary={}) -> void:
 	print('FreeLook Camera')
 
-func update(_delta) -> void:
+func handle_input(_event) -> void:
 	if not Input.is_action_pressed("free-look"):
 		emit_signal("change_state_request", "Default")
